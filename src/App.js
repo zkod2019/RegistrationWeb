a3faadf7afdb8dd933bb5105a80edf27e888f354
Ahmed Macan Markar <aamm723@uowmail.edu.au>
2022-11-11 11:39:16 +0400

    thischange

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import img from './assembly-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="sub-main">
+        <div className='imgs'>
+        
+        <img src={img} alt="logo"/>
+        
+        </div>
+        <h1></h1>
+        {/* <div>
+        
+         <div>
+
+           {/* <input type="text" placeholder='Email' className='email'/> 
+          </div>
+
+  </div> */}
+      </div>
+   
     </div>
   );
 }
